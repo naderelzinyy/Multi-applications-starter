5801b073d38d7f64423903bb7a863f375a9492e6
naderelzinyy <naderelziny805@gmail.com>
2022-03-19 01:44:37 +0300

    Adding Frame and canvas

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-
-import tkinter as tkinter
+import tkinter as tk
 from tkinter import filedialog, Text
 import os
 
-root = tkinter.Tk()
+root = tk.Tk()
+# Canvas
+canvas = tk.Canvas(root, height=700, width=900, bg='#1f1f1f')
+canvas.pack()
+# Frames
+frame = tk.Frame(canvas, bg='white')
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1 )
 
 root.mainloop()
